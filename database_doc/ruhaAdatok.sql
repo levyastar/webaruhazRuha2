a2ab1e22b667646a5f1c358e5372e45c2323a371
levyastar <20030531levi@gmail.com>
2023-02-21 13:06:59 +0100

    kezdes

---
+++ database_doc/ruhaAdatok.sql
@@ -0,0 +1,22 @@
+INSERT INTO poloadatok (id, meret, szín, marka,ar) VALUES
+  (1, 'S', 'Fekete', 'Nike Sportswear Tee', 9800),
+  (2, 'XS','Fekete', 'Nike Repeat Tee', 12500),
+  (3, 'M', 'Fehér', 'Nike Club Tee', 8900),
+  (4, 'XXS','Piros', 'Nike Academy Tee', 7800),
+  (5, 'L', 'Fekete', 'Nike NSW Tee', 8230),
+  (6, 'XXL', 'Fekete', 'Nike Sportswear Essentials', 15800);
+
+
+INSERT INTO nadragok (id,meret,szín,marka,ar) VALUES
+   (1, 'XS', 'Fekete', 'Nike Tech Fleece Pants', 38000),
+   (2, 'S', 'Szürke', 'Nike Club Pants', 18000),
+   (3, 'M', 'Fekete', 'Nike Swoosh Pants', 24500),
+   (4, 'L', 'Fehér', 'Nike Sportswear Cargo Pants', 23000);
+
+INSERT INTO puloverek (id, meret, szín, marka, ar) VALUES
+   (1, 'XS', 'Fekete', 'Nike Tech Fleece Hoodie', 45000),
+   (2, 'S', 'Piros', 'Nike Club Fleece Hoodie', 24000),
+   (3, 'M', 'Fehér', 'Nike Swoosh Hoodie', 32000),
+   (4, 'L', 'Fekete', 'Nike Sportswear Trainer Hoodie', 18000);
+
+
